9571ce7231feee23fd6f324377b6e233dec4bc8b
lottenoorlander <lottenoorlander@gmail.com>
2019-11-18 11:36:24 +0100

    fooled around a bit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <p>I'm just learning about react I wonder what this does??? What will this do?</p>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        "Hello Lotty!"
         </a>
-      </header>
-    </div>
+    </div >
   );
 }
 
