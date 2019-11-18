fab84dce3aa464f7f7b2e2e514e9fad8a6413095
lottenoorlander <lottenoorlander@gmail.com>
2019-11-18 11:44:39 +0100

    changed some text

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>I'm just learning about react I wonder what this does??? What will this do?</p>
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        "Hello Lotty!"
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Welcome to Codaisseur
         </a>
-    </div >
+      </header>
+    </div>
   );
 }
 
